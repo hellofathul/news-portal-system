00b440870e57f47bd94e9c2b9235db1b2288423d
amin-nematix <amin@nematix.com>
2024-05-27 22:32:29 +0800

    continue implement multi authentication system with laravel breeze

--- routes/admin.php
+++ routes/admin.php
@@ -1,4 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\AdminAuthenticationController;
+
+Route::group(["prefix" => "/admin", "as" => "admin."], function () {
+    Route::get("/login", [AdminAuthenticationController::class, "login"])->name("login");
+});
+
+Route::group(["prefix" => "/admin", "as" => "admin.", "middleware" => ["admin"]], function () {
+    Route::get("/dashboard", [DashboardController::class, "index"])->name("dashboard");
+});
+
 
